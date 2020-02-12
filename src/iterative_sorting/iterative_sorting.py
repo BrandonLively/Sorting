d13210f6e434fc3e54af1d8ccffe0c585eb932ef
BrandonLively <akutenshi@live.com>
2020-02-12 16:15:10 -0500

    Fixes bubble sort (pointer issue made me think I was done)

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -22,12 +22,13 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     counter = 0
-    for i in range(1, len(arr) - counter):
-        if arr[i] < arr[i - 1]:
-            pos1 = arr[i - 1]
-            pos2 = arr[i]
-            arr[i - 1] = pos2
-            arr[i] = pos1
+    for i in range(1, len(arr)):
+        for j in range(1, len(arr) - counter):
+            if arr[j] < arr[j - 1]:
+                pos1 = arr[j - 1]
+                pos2 = arr[j]
+                arr[j - 1] = pos2
+                arr[j] = pos1
     return arr
 
 
@@ -36,9 +37,13 @@ def count_sort(arr, maximum=-1):
     return arr
 
 
+
+
+
+
 test_arr = [7, 6, 56, 3, 21, 2, 45, 23, 34, 6, 86, 62134, 3, 35, 24, 5, 2, 42, 6, 23, 4, 6, 2345, 23, 6523, 42, 23523,
             457, 54, 325, 23, 523, 5]
-
+print(bubble_sort(test_arr))
 print(selection_sort(test_arr))
 
-print(bubble_sort(test_arr))
+
