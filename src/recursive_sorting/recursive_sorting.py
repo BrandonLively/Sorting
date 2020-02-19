5fed8db9f4e580d9f6a48c90d14653d432cc45dd
BrandonLively <akutenshi@live.com>
2020-02-19 02:24:02 -0500

    Cleaned up Merge and Merge Sort functions

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,9 +1,7 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge(arrA, arrB):
     merged_arr = [0] * (len(arrA) + len(arrB))  # TO-DO
-    i = 0
-    j = 0
-    counter = 0
+    i, j, counter = 0, 0, 0
     arr_a_complete = False
     while i < len(arrA) and j < len(arrB):
         if arrA[i] < arrB[j]:
@@ -18,15 +16,9 @@ def merge(arrA, arrB):
             arr_a_complete = False
 
     if arr_a_complete:
-        while j < len(arrB):
-            merged_arr[counter] = arrB[j]
-            j += 1
-            counter += 1
+        merged_arr[counter:] = arrB[j:]
     else:
-        while i < len(arrA):
-            merged_arr[counter] = arrA[i]
-            i += 1
-            counter += 1
+        merged_arr[counter:] = arrA[i:]
     return merged_arr
 
 
@@ -34,12 +26,9 @@ def merge(arrA, arrB):
 def merge_sort(arr):
     # TO-DO
     if len(arr) > 1:
-        arrA = arr[:len(arr) // 2]
-        arrB = arr[len(arr) // 2:]
-        return merge(merge_sort(arrA),merge_sort(arrB))
+        return merge(merge_sort(arr[:len(arr) // 2]), merge_sort(arr[len(arr) // 2:]))
     else:
         return arr
-
 
 
 # STRETCH: implement an in-place merge sort algorithm
